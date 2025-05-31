18ebda1350e31812eeb4d72d0036be625e8834a2
Prem Wannakboth <wannakboth.p@bidc.com.kh>
2025-05-31 16:36:07 +0700

    update

--- lib/src/get_package_version.dart
+++ lib/src/get_package_version.dart
@@ -1,15 +1,32 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:yaml/yaml.dart';
 import 'package:path/path.dart' as p;
+import 'package:yaml/yaml.dart';
 
 String getPackageVersion() {
-  // Locate the package's own pubspec.yaml even when used as a dependency
-  final packageDir = File.fromUri(Platform.script).parent.parent.parent;
-  final pubspec = File(p.join(packageDir.path, 'pubspec.yaml'));
+  try {
+    final packageConfigFile = File(p.join(Directory.current.path, '.dart_tool/package_config.json'));
+    if (!packageConfigFile.existsSync()) return 'Unknown';
 
-  if (!pubspec.existsSync()) return 'Unknown';
+    final configJson = jsonDecode(packageConfigFile.readAsStringSync());
+    final packages = configJson['packages'] as List;
 
-  final content = pubspec.readAsStringSync();
-  final doc = loadYaml(content);
-  return doc['version'] ?? 'Unknown';
+    final autoLauncherPackage = packages.firstWhere(
+      (p) => p['name'] == 'auto_launcher',
+      orElse: () => null,
+    );
+
+    if (autoLauncherPackage == null) return 'Unknown';
+
+    final rootUri = autoLauncherPackage['rootUri'] as String;
+    final pathToPubspec = p.join(Directory.current.path, rootUri, 'pubspec.yaml');
+    final pubspec = File(pathToPubspec);
+    if (!pubspec.existsSync()) return 'Unknown';
+
+    final content = pubspec.readAsStringSync();
+    final doc = loadYaml(content);
+    return doc['version'] ?? 'Unknown';
+  } catch (_) {
+    return 'Unknown';
+  }
 }
