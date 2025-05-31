a7ce8cecdd14c1adbfc8c569ced36576247a5c98
Prem Wannakboth <wannakboth.p@bidc.com.kh>
2025-05-31 18:26:29 +0700

    push

--- lib/src/get_package_version.dart
+++ lib/src/get_package_version.dart
@@ -5,28 +5,30 @@ import 'package:yaml/yaml.dart';
 
 String getPackageVersion() {
   try {
-    final packageConfigFile = File(p.join(Directory.current.path, '.dart_tool/package_config.json'));
-    if (!packageConfigFile.existsSync()) return 'Unknown';
+    final configFile = File(p.join('.dart_tool', 'package_config.json'));
+    if (!configFile.existsSync()) return 'Unknown';
 
-    final configJson = jsonDecode(packageConfigFile.readAsStringSync());
-    final packages = configJson['packages'] as List;
+    final configJson = jsonDecode(configFile.readAsStringSync());
+    final packages = configJson['packages'] as List<dynamic>;
 
-    final autoLauncherPackage = packages.firstWhere(
-      (p) => p['name'] == 'auto_launcher',
+    final thisPackage = packages.firstWhere(
+      (pkg) => pkg['name'] == 'auto_launcher',
       orElse: () => null,
     );
 
-    if (autoLauncherPackage == null) return 'Unknown';
+    if (thisPackage == null) return 'Unknown';
 
-    final rootUri = autoLauncherPackage['rootUri'] as String;
-    final pathToPubspec = p.join(Directory.current.path, rootUri, 'pubspec.yaml');
-    final pubspec = File(pathToPubspec);
-    if (!pubspec.existsSync()) return 'Unknown';
+    final rootUri = thisPackage['rootUri'] as String;
+    final absolutePath = p.normalize(p.join(Directory.current.path, rootUri));
+    final pubspecPath = p.join(absolutePath, 'pubspec.yaml');
 
-    final content = pubspec.readAsStringSync();
-    final doc = loadYaml(content);
-    return doc['version'] ?? 'Unknown';
-  } catch (_) {
+    final pubspecFile = File(pubspecPath);
+    if (!pubspecFile.existsSync()) return 'Unknown';
+
+    final content = pubspecFile.readAsStringSync();
+    final yaml = loadYaml(content);
+    return yaml['version']?.toString() ?? 'Unknown';
+  } catch (e) {
     return 'Unknown';
   }
 }
